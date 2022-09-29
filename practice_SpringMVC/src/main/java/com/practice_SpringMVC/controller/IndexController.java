c14c328324e3a887b6b6243620ad258bc53ec8d7
macaroro <cho_0826@naver.com>
2022-09-29 20:56:03 +0900

    2022-09-29(spring framework  dispather-servlet 설정 )

---
+++ practice_SpringMVC/src/main/java/com/practice_SpringMVC/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.practice_SpringMVC.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class IndexController implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println(">>>>");
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("data","µÇ³ª¿ä");
+	
+		return mv;
+	}
+	
+
+}
